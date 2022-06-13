e1d0dd981a5fe2646ac98fcdb47e23365a1c5dd4
Alice Machado Koch <machado.alice.koch@gmail.com>
2022-06-13 19:57:15 -0300

    #0020 - Ajustes caminho historicos aulas

---
+++ src/components/molecules/proximaAula/historicoAulas/HistoricoAulas.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+export default function TituloProximaAulaIcone() {
+
+    return (
+        <View style={styles.container}>
+            
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        height:'100%',
+        width: '100%',
+        alignItems: 'center'
+    }
+});
